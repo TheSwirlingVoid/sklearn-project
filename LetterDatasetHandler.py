5a07694b7ac3f533318726ab39946453799f31cb
TheSwirlingVoid <rnsugavanam@gmail.com>
2022-11-02 12:17:11 -0700

    Abstracted some more CSV stuff behind the LetterDatasetHandler object

--- LetterDatasetHandler.py
+++ LetterDatasetHandler.py
@@ -9,26 +9,26 @@ import numpy as np
 import pandas
 import cv2 as cv
 
-DATASET_FOLDER = "archive/letters2"
+DATASET_FOLDER = "archive/letters3"
 
 class LetterDatasetHandler():
 
-	def __init__(self, datafile) -> None:
+	def __init__(self, datafile, paths_column_name, targets_column_name) -> None:
 		self.datafile = datafile
-		self.data = None
-		self.csv = None
+		self.data = Bunch()
+
+		self.csv = pandas.read_csv(self.datafile)
+		self.column_name = paths_column_name
+		self.image_paths = self.csv[self.column_name]
+		self.targets = self.csv[targets_column_name]
 # ---------------------------------------------------------------------------- #
 	def load_data(self) -> None:
-		self.data = Bunch()
-		
-		self.csv = pandas.read_csv(self.datafile)
 		# add folder prefix to prime image reading
-		self.csv["file"] = DATASET_FOLDER+"/"+self.csv["file"]
-		paths = self.csv["file"].to_numpy()
+		self.image_paths = DATASET_FOLDER+"/"+self.image_paths
+		paths = self.image_paths.to_numpy()
 		#-----------------------------------------------------#
 		self.data["features"] = self.csv.columns.values
 		self.data["class_targets"] = np.empty((0))
-		self.data["possible_results"] = self.csv["letter"].unique()
 
 		self.data["images"] = \
 			self.__paths_to_image_array(paths)
@@ -50,7 +50,7 @@ class LetterDatasetHandler():
 			if image.shape[1] == x_dim and image.shape[0] == y_dim:
 				# brackets around image to add a dimension (2->3)
 				img_array = np.append(img_array, [image], axis=0)
-				self.__add_class_target(self.csv["letter"][idx])
+				self.__add_class_target(self.targets[idx])
 
 		return img_array
 # ---------------------------------------------------------------------------- #
@@ -61,7 +61,7 @@ class LetterDatasetHandler():
 	def __flatten_image_array(self, img_array):
 		return \
 		self.data["images"].reshape(
-			(img_array.shape[0], img_array.shape[1]*img_array.shape[2])
+			(img_array.shape[0], None)
 		)
 # ---------------------------------------------------------------------------- #
 	def __get_gray_img(self, path) -> None:
@@ -69,41 +69,43 @@ class LetterDatasetHandler():
 		image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 		return image
 # ---------------------------------------------------------------------------- #
-	def get_data(self) -> Bunch | None:
-		return self.data
+	def get_input_data(self) -> Bunch | None:
+		return self.data["flattened_images"]
+# ---------------------------------------------------------------------------- #
+	def get_targets(self) -> Bunch | None:
+		return self.data["class_targets"]
 # ---------------------------------------------------------------------------- #
 
 def main() -> None:
 
-	dh = LetterDatasetHandler(DATASET_FOLDER+".txt")
+	dh = LetterDatasetHandler(DATASET_FOLDER+".txt", "file", "letter")
 	dh.load_data()
 
-	data = dh.get_data()
+	input_data = dh.get_input_data()
+	targets = dh.get_targets()
 
-	npredict = 10
+	npredict = 100
 
 	# estimator
 	clf = svm.SVC()
 
 	train_data, test_data, train_sol, test_sol = \
 		train_test_split(
-			data["flattened_images"], data["class_targets"],
+			input_data, targets,
 			test_size=npredict,
 			random_state=142 # some random random seed or something idk
 		)
 
+	print("Training with " + str(len(train_data)) + " entries")
 	# num-of-images samples, num-of-pixels features
 	clf.fit(train_data, train_sol)
 
 	print("Training done!")
 
-	print("Possible results:")
-	print(data["possible_results"])
-
 	predictions = clf.predict(test_data)
 
 	cm = confusion_matrix(test_sol, predictions)
-	cm_display = ConfusionMatrixDisplay(cm).plot()
+	ConfusionMatrixDisplay(cm).plot()
 	plt.show()
 # ---------------------------------------------------------------------------- #
 	# while True:
